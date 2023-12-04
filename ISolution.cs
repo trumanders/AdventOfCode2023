147cd7d91e6aca560b6ae71c808178f30314bdf9
Anders Johansson <trumanders@gmail.com>
2023-12-04 15:15:38 +0100

    Dec01 finished

--- ISolution.cs
+++ ISolution.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2023;
+﻿namespace AdventOfCode2023;
 
 public interface ISolution
 {
+    public string Input { get; set; }
+    public int Answer1 { get; set; }
+    public int Answer2 { get; set; }
     public void Solve();
+    public void Solve1();
+    public void Solve2();
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,29 @@
         {
             List<ISolution> allSolutions = new List<ISolution>();
             allSolutions.Add(new Dec01());
-            allSolutions.Add(new Dec02());
-            allSolutions.Add(new Dec03());
-            allSolutions.Add(new Dec04());
-            allSolutions.Add(new Dec05());
-            allSolutions.Add(new Dec06());
-            allSolutions.Add(new Dec07());
-            allSolutions.Add(new Dec08());
-            allSolutions.Add(new Dec09());
-            allSolutions.Add(new Dec10());
-            allSolutions.Add(new Dec11());
-            allSolutions.Add(new Dec12());
-            allSolutions.Add(new Dec13());
-            allSolutions.Add(new Dec14());
-            allSolutions.Add(new Dec15());
-            allSolutions.Add(new Dec16());
-            allSolutions.Add(new Dec17());
-            allSolutions.Add(new Dec18());
-            allSolutions.Add(new Dec19());
-            allSolutions.Add(new Dec20());
-            allSolutions.Add(new Dec21());
-            allSolutions.Add(new Dec22());
-            allSolutions.Add(new Dec23());
-            allSolutions.Add(new Dec24());
+            //allSolutions.Add(new Dec02());
+            //allSolutions.Add(new Dec03());
+            //allSolutions.Add(new Dec04());
+            //allSolutions.Add(new Dec05());
+            //allSolutions.Add(new Dec06());
+            //allSolutions.Add(new Dec07());
+            //allSolutions.Add(new Dec08());
+            //allSolutions.Add(new Dec09());
+            //allSolutions.Add(new Dec10());
+            //allSolutions.Add(new Dec11());
+            //allSolutions.Add(new Dec12());
+            //allSolutions.Add(new Dec13());
+            //allSolutions.Add(new Dec14());
+            //allSolutions.Add(new Dec15());
+            //allSolutions.Add(new Dec16());
+            //allSolutions.Add(new Dec17());
+            //allSolutions.Add(new Dec18());
+            //allSolutions.Add(new Dec19());
+            //allSolutions.Add(new Dec20());
+            //allSolutions.Add(new Dec21());
+            //allSolutions.Add(new Dec22());
+            //allSolutions.Add(new Dec23());
+            //allSolutions.Add(new Dec24());
 
             foreach (ISolution solution in allSolutions)
             {
