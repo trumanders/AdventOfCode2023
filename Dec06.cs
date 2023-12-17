f2b47cfa98d4194fca8786a5f07bb58abb4872a9
Anders Johansson <trumanders@gmail.com>
2023-12-17 17:34:14 +0100

    Dec05 : 1 and Dec06 finshed

--- Dec06.cs
+++ Dec06.cs
@@ -10,7 +10,7 @@ public class Dec06 : ISolution
 
     public Dec06()
     {
-
+        Input = "60 475 94 2138 78 1015 82 1650";
     }
 
     public void Solve()
@@ -21,11 +21,34 @@ public class Dec06 : ISolution
 
     public void Solve1()
     {
+        var inputs = Input.Split(' ');
+        Answer1 = 1;
+        for (int i = 0; i < inputs.Length; i += 2)
+        {
+            int winCount = 0;
+            int duration = Convert.ToInt32(inputs[i]);
+            int distance = Convert.ToInt32(inputs[i + 1]);
+            
+            for (int buttonHoldSec = 0; buttonHoldSec <= duration; buttonHoldSec++)
+            {
+                if ((duration - buttonHoldSec) * buttonHoldSec > distance)
+                    winCount++;
+            }
+            Answer1 *= winCount;
+        }
         Console.WriteLine("First: \t" + Answer1);
     }
 
     public void Solve2()
-    {
+    {        
+        long duration = 60947882;
+        long distance = 475213810151650;
+        long buttonHoldInMsStart = distance / duration;
+        for (int buttonHoldSec = 0; buttonHoldSec <= duration; buttonHoldSec++)
+        {
+            if ((duration - buttonHoldSec) * buttonHoldSec > distance)
+                Answer2++;
+        }
         Console.WriteLine("Second:\t" + Answer2);
     }
 }
